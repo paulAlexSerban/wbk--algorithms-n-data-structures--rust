de6247514fd36b64dbbd6936fac111062fadee52
paulAlexSerban <paul.alex.serban@gmail.com>
2022-12-19 22:16:39 +0200

    added some string exercises

--- 00__basics-n-fundamentals/00__hello-rust/src/main.rs
+++ 00__basics-n-fundamentals/00__hello-rust/src/main.rs
@@ -1,3 +1,23 @@
+// Silence some warnings so they don't distract from the exercise.
+#![allow(unused_assignments)]
+
 fn main() {
-    println!("Hello, world!");
+    hello_world("Paul")
 }
+
+fn hello_world(name: &str) {
+    let str = String::from("Hello, world!");
+    let lng = String::from("Rust");
+
+    // create new string
+    let mut str_date = String::new();
+
+    // declare the string literal
+    let date = "December 2022 ;)";
+
+    // create a String from a string literal
+    str_date = date.to_string();
+    println!("{} I am {}, and I am learning {} in {}", str, name, lng, str_date);
+}
+
+// in VSC, right click + Run Code
